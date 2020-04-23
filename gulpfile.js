e5ebfadd4d0bad5ec18d3159140b2714133aeb2b
Gilfoyle1 <kostomarov.e@gmail.com>
2020-04-23 15:24:19 +0300

    ошибка в server

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task("server", function () {
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/img/sprite-*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/*.html").gulp.series("html", "refresh");
+  gulp.watch("source/*.html"), gulp.series("html", "refresh");
 });
 
 gulp.task("images", function () {
