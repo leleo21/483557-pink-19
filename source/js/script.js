1970e66e4ea866110003b0feaacd208a4bf26643
Gilfoyle1 <kostomarov.e@gmail.com>
2020-04-27 14:00:48 +0300

    РР всех страниц, кроме фото

--- source/js/script.js
+++ source/js/script.js
@@ -16,3 +16,32 @@ navToggle.addEventListener('click', function () {
     navToggle.classList.remove('page-header__toggle--opened');
   }
 });
+
+// модалки
+
+var modalSuccess = document.querySelector(".modal--success");
+var modalError = document.querySelector(".modal--error");
+var modalClose = document.querySelector(".modal__btn");
+var modal = document.querySelector(".modal");
+var surname = document.querySelector("[name=surname]");
+var name = document.querySelector("[name=name]");
+var email = document.querySelector("[name=email]");
+var form = document.querySelector(".contest__form");
+
+
+
+form.addEventListener("submit", function (evt) {
+  if (!surname.value || !name.value || !email.value) {
+    evt.preventDefault();
+    modalError.classList.add("modal__error-show");
+  } else {
+    evt.preventDefault();
+    modalSuccess.classList.add("modal__success-show");
+  }
+});
+
+
+modalClose.addEventListener("click", function () {
+  modal.classList.remove("modal__error-show");
+  modal.classList.remove("modal__success-show");
+});
